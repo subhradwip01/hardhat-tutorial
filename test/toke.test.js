cd1ae78653d6bd9575dcdf3c97224b9c498d89aa
subhradwip01 <subhradwip01@gmail.com>
2022-11-08 21:15:54 +0530

    Should transfer tokens between accounts test done

--- test/toke.test.js
+++ test/toke.test.js
@@ -11,4 +11,23 @@ describe("Token contract", function () {
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
+
+
+  it("Should transfer tokens between accounts", async function (){
+    const [owner,addr1,addr2]= await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    await hardhatToken.transfer(addr1.address,10);
+
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
+
+    // transfer 5 tokens from addr1 to addr2 
+    await hardhatToken.connect(addr1).transfer(addr2.address,6);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(6);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(4);
+  })
+
 });
